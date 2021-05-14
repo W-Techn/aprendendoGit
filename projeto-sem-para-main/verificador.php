890d91d2f05a17f1c7981138de95612e3d6fbcb8
Marconi Afonso <77650431+kaduzitoh@users.noreply.github.com>
2021-05-14 14:58:09 -0300

    Implementação de Request
    O API busca o número chave  e retorna o nome na página de usuário caso esse valor esteja na API.

--- projeto-sem-para-main/verificador.php
+++ projeto-sem-para-main/verificador.php
@@ -7,64 +7,103 @@ require_once 'src/CNPJ.php';
 require_once 'src/CPF.php';
 
 $numero = filter_input(INPUT_POST, 'chave', FILTER_SANITIZE_NUMBER_FLOAT); //o conteúdo de cada lacuna preenchida deverá ser armazenada em uma variável. Para isso, deverá ser usada a função filter_input, que recebe como parâmetro:
-$Formatado = str_split($numero); //transformará a variável cpf em uma lista 
+$list = str_split($numero); //transformará a variável cpf em uma lista 
 
-$sql= "SELECT * FROM cadastros where Registros = '$numero'";
+$postfields = array('cpf' => $numero);
 
-switch (sizeof($Formatado)) {
-    case 12:
-        unset($Formatado[9]); //retira o traço do CPF
-        $numerosemtraco = array_values($Formatado); //rearranja a lista após a deleção anterior
+switch (sizeof($list)) {
+    case 11:
 
-        $cpfAtestar = new CPF($numerosemtraco); //após todas esseas transformações, vamos passar essa nova lista como parâmetro na construção de um objeto
+    $cpfAtestar = new CPF($list); //após todas esseas transformações, vamos passar essa nova lista como parâmetro na construção de um objeto
 
     if ($cpfAtestar->segundodigitoehValido() == true) {
+        $curl = curl_init();
 
-        $resultado_usuario = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($resultado_usuario) > 0) {
-                sleep(3.5);
+        curl_setopt_array($curl, [
+        CURLOPT_URL => "http://172.22.11.31/w-api/semparar/dadosDevedor",
+        CURLOPT_RETURNTRANSFER => true,
+        CURLOPT_ENCODING => "",
+        CURLOPT_MAXREDIRS => 10,
+        CURLOPT_TIMEOUT => 30,
+        CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+        CURLOPT_CUSTOMREQUEST => "POST",
+        CURLOPT_POSTFIELDS => "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"cpf\"\r\n\r\n$numero\r\n-----011000010111000001101001--\r\n",
+        CURLOPT_HTTPHEADER => [
+            "Content-Type: multipart/form-data; boundary=---011000010111000001101001"
+        ],
+        ]);
 
-                while($row = mysqli_fetch_assoc($resultado_usuario)) {
-                $user = $row['Nome'];
-                }
-                $_SESSION['msg2'] = "<p style='color:#1aff00; font-size:1.1rem;'>É um prazer te receber $user!</p>
-                <button>Gerar boleto</button>";
-                header("Location: user.php");
-                
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+        if (strlen($response) > 169) {
+            sleep(3.5);
+            $encode = explode(",", $response, 7);
+            $name = $encode[5];
+            $proper = explode('"', $name);
+            unset($proper['"']);
+            $str = $proper[3];
+            $nombre = mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
+            
+            $_SESSION['msg2'] = "<p style='color:#1aff00; font-size:1.1rem;'>É um prazer te receber $nombre!</p>
+            <button>Gerar boleto</button>";
+            header("Location: user.php");
             } else {
-                sleep(5);
-                $_SESSION['msg'] = "*CPF não encontrado";
-                header("Location: index.php");
-            }    
+            sleep(5);
+            $_SESSION['msg'] = "*CPF não encontrado";
+            header("Location: index.php");
+            
+        }
+        
         }  else { //se a o segundo dígito não passar na verificação,
-            $_SESSION['msg'] = "*Valor inválido!"; //significa que ele não existe, e é isso que nós passaremos a imprimir na nova seção que será aberta, no redirecionamento que haverá
+            $_SESSION['msg'] = "*Esse CPF não existe!"; //significa que ele não existe, e é isso que nós passaremos a imprimir na nova seção que será aberta, no redirecionamento que haverá
             header("Location: index.php"); //para a página Index;
         }
         break;
-        case 15:
-            unset($Formatado[12]); //retira o traço do CPF
-            $numerosemtraco = array_values($Formatado); //rearranja a lista após a deleção anterior
+        case 14:
     
-            $cnpjAtestar = new CNPJ($numerosemtraco); //após todas esseas transformações, vamos passar essa nova lista como parâmetro na construção de um objeto
+            $cnpjAtestar = new CNPJ($list); //após todas esseas transformações, vamos passar essa nova lista como parâmetro na construção de um objeto
             if ($cnpjAtestar->segundoDigitoCNPJehValido() == true) {
+                $curl = curl_init();
 
-                $resultado_usuario = mysqli_query($conn, $sql);
-                    if (mysqli_num_rows($resultado_usuario) > 0) {
-                        sleep(3.5);
-
-                        while ($row = mysqli_fetch_assoc($resultado_usuario)) {
-                            $user = $row['Nome'];
-                        }
-                        $_SESSION['msg2'] = "<p style='color:#1aff00; font-size:1.1rem;'>É um prazer te receber $user!</p>
-                        <button>Gerar boleto</button>";
-                        header("Location: user.php");    
-                    } else {
-                        sleep(5);
-                        $_SESSION['msg'] = "*CNPJ não encontrado";
-                        header("Location: index.php");
-                    }    
+                curl_setopt_array($curl, [
+                CURLOPT_URL => "http://172.22.11.31/w-api/semparar/dadosDevedor?cpf=11000248836",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => "-----011000010111000001101001\r\nContent-Disposition: form-data; name=\"cpf\"\r\n\r\n$numero\r\n-----011000010111000001101001--\r\n",
+                CURLOPT_HTTPHEADER => [
+                    "Content-Type: multipart/form-data; boundary=---011000010111000001101001"
+                ],
+                ]);
+        
+                $response = curl_exec($curl);
+                $err = curl_error($curl);
+        
+                curl_close($curl);
+                if (strlen($response) > 169) {
+                    sleep(3.5);
+                    $encode = explode(",", $response, 7);
+                    $name = $encode[5];
+                    $proper = explode('"', $name);
+                    unset($proper['"']);
+                    $str = $proper[3];
+                    $nombre = mb_convert_case($str, MB_CASE_TITLE, "UTF-8");
+                    $_SESSION['msg2'] = "<p style='color:#1aff00; font-size:1.1rem;'>É um prazer te receber $nombre!</p>
+                    <button>Gerar boleto</button>";
+                    header("Location: user.php");
+                    
+                } else {
+                    sleep(5);
+                    $_SESSION['msg'] = "*CNPJ não encontrado";
+                    header("Location: index.php");
+                }
                 }  else { //se a o segundo dígito não passar na verificação,
-                    $_SESSION['msg'] = "*Valor inválido!"; //significa que ele não existe, e é isso que nós passaremos a imprimir na nova seção que será aberta, no redirecionamento que haverá
+                    $_SESSION['msg'] = "*Esse CNPJ não existe!"; //significa que ele não existe, e é isso que nós passaremos a imprimir na nova seção que será aberta, no redirecionamento que haverá
                     header("Location: index.php"); //para a página Index;
                 }                
         break;
